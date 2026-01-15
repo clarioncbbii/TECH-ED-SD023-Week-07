4d84342bca7a19bad6a582ece995d6ee0d71466c
Nene Nnoruka <237324102+clarioncbbii@users.noreply.github.com>
2026-01-15 17:08:07 +0000

    feat: start of server .env and dBConnection setup"

--- server/dbConnection.js
+++ server/dbConnection.js
@@ -1,1 +1,15 @@
 //TODO: Set up a db pool
+
+// imports
+import dotenv from "dotenv";
+import pg from "pg";
+
+// config dotenv
+dotenv.config();
+
+//create a pool with pg
+const db = new pg.Pool({
+  connectionString: process.env.DATABASE_URL,
+});
+
+export { db };
