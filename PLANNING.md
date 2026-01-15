be96d19256d76f94903843cf7f36ce0103731d8e
Nene Nnoruka <237324102+clarioncbbii@users.noreply.github.com>
2026-01-15 17:03:36 +0000

    chore: routine eo day

--- PLANNING.md
+++
@@ -1,8 +0,0 @@
-<!-- ============== TIPS FROM MANNY ============== -->
-
-- Plan out your assignment (both client and server) - PLANNING IS KEY!
-- Relational databases are not a requirement for this assignment. We CAN have multiple tables, but we do NOT HAVE to.
-- If you want to do stretch goals, start with DELETE route and the DYNAMIC routes
-- Practice Git version control
-
-<!-- ============== PLANNING TEMPLATE ============== -->
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -13,6 +13,8 @@ function App() {
 
 export default App;
 
+// ================================================================ //
+
 //*Note: In the componenets folder you have the minimum amount of componenets to make your app work, but I really recommend you make the most of the component system (use props!!!)
 
-//It's not a requirement but I think the users would like some conditional rendering to make the UI less cluttered...
+//Extra: Though it's not a requirement but I think the users would like some conditional rendering to make the UI less cluttered...
