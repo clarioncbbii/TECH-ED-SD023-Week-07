555b56499371b4a0c095152952d337dfaa86fa8f
Nene Nnoruka <237324102+clarioncbbii@users.noreply.github.com>
2026-01-16 12:06:58 +0000

    push to fix server and client deploy failures

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,7 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
-import { BrowserRouter } from "react-router";
 
 //TODO: I want react router to be available in my App once installed
 //We have to give access to the package using a provider
@@ -9,8 +8,6 @@ import { BrowserRouter } from "react-router";
 //we are nesting App within BrowserRouter so App has access to the react router tools
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <App />
   </StrictMode>
 );
