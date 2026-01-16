9b8b83940cb499cfd39f70595a4d141fab27419a
Nene Nnoruka <237324102+clarioncbbii@users.noreply.github.com>
2026-01-16 11:53:03 +0000

    feat: server config

--- server/queries.sql
+++ server/queries.sql
@@ -1,3 +1,38 @@
 --! This file is a reminder to submit:
--- Schema visualiser screenshot
--- SQL queries from your editor on Supabase
+
+--* Schema visualiser screenshot 
+see './database-schema.jpg'
+
+--* SQL queries from your editor on Supabase
+
+--TODO: Create table called 'recipes'
+CREATE TABLE IF NOT EXISTS recipes (
+  id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  recipe_name VARCHAR,
+  ingredients VARCHAR,
+  recipe_time INT,
+  method TEXT,
+  image_link TEXT
+);
+
+--TODO: Seed table
+INSERT INTO recipes (recipe_name, ingredients, recipe_time, method, image_link) VALUES 
+('Jollof rice', 'rice, tomatoes, onions, garlic', 120, 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi soluta quas obcaecati, et cupiditate aspernatur provident ratione aliquam fugit sunt officiis. Assumenda est iure maiores temporibus dolorum rerum explicabo deserunt?', 'https://static01.nyt.com/images/2024/08/07/multimedia/05FAMILY-REUNIONREX3-jollof-rice-kjhg/FAMILY-REUNIONREX3-jollof-rice-kjhg-mediumSquareAt3X.jpg'),
+('Egusi soup', 'egusi, spinach, scotch bonnet', 180, 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi soluta quas obcaecati, et cupiditate aspernatur provident ratione aliquam fugit sunt officiis. Assumenda est iure maiores temporibus dolorum rerum explicabo deserunt?', 'https://simshomekitchen.com/wp-content/uploads/2025/09/Egusi-in-a-bowl-with-assorted-meat.jpg'),
+('Puff puff', 'flour, sugar, butter, evaporated milk', 60, 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi soluta quas obcaecati, et cupiditate aspernatur provident ratione aliquam fugit sunt officiis. Assumenda est iure maiores temporibus dolorum rerum explicabo deserunt?', 'https://www.seriouseats.com/thmb/Nu0tR17GqQjiNALnk4WWKHAIlbI=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/20220705-PuffPuffMaureen-Celestine-21-b365ebc25e3d4dde978c02c19cc5b2cd.JPG'),
+('Meat pie', 'beef mince, flour, butter, potato, carrot, onion', 90, 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi soluta quas obcaecati, et cupiditate aspernatur provident ratione aliquam fugit sunt officiis. Assumenda est iure maiores temporibus dolorum rerum explicabo deserunt?', 'https://www.dashofjazz.com/wp-content/uploads/2024/03/Dash-of-Jazz-Nigerian-Meat-Pies-18.jpg'),
+('Tomato stew', 'tomato, onion, scotch bonnet, oil, meat of choice', 150, 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi soluta quas obcaecati, et cupiditate aspernatur provident ratione aliquam fugit sunt officiis. Assumenda est iure maiores temporibus dolorum rerum explicabo deserunt?', 'https://www.myactivekitchen.com/wp-content/uploads/2019/05/nigerian-beef-stew_image_10.jpg'),
+('Goat pepper soup', 'pepper soup spice mix, stock cubes, goat/mutton', 150, 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi soluta quas obcaecati, et cupiditate aspernatur provident ratione aliquam fugit sunt officiis. Assumenda est iure maiores temporibus dolorum rerum explicabo deserunt?', 'https://canadabeef.ca/wp-content/uploads/2025/02/Nigerian-Beef-Pepper-Soup4-1030x773.jpg') RETURNING *
+
+--TODO: CREATE a new recipe ---> POST method 
+INSERT INTO recipes (recipe_name, ingredients, recipe_time, method, image_link) VALUES ($1, $2, $3,$4, $5) RETURNING *, [recipe_name, ingredients, recipe_time, method, image_link]
+
+--TODO: READ data in the table --> GET method ("/all-recipes")
+SELECT recipe_name, ingredients, recipe_time, method, image_link FROM recipes;
+
+--TODO: READ data in the table --> GET method ("/all-recipes/:id")
+SELECT recipe_name, ingredients, recipe_time, method, image_link FROM recipes WHERE id = $1
+
+
+
+
--- server/server.js
+++ server/server.js
@@ -1,6 +1,94 @@
 //TODO: Set up a server API
 
+// imports
+import express from "express";
+import cors from "cors";
+import { db } from "./dbConnection.js";
+
+// initialise express
+const app = express();
+
+// use JSON in our server
+app.use(express.json());
+
+// config cors
+app.use(cors());
+
+// port setup
+const PORT = 8080;
+
+app.listen(PORT, () => {
+  console.info(`Server API is live on ${PORT}!`);
+});
+
 // ===================
 
 //TODO: Set up a routing system with at least one GET route and one POST route
 //remember to store your secrets in the .env file
+
+//* ROOT route
+// route --> http method GET b/c we want to READ data
+
+app.get("/", (req, res) => {
+  res.json({ message: "Hawaiyu - welcome to the server API!" });
+});
+
+//* GET ROUTE
+// READ recipes data
+// static route --> http method GET
+// use POSTMAN to test this route is working properly
+
+app.get("/all-recipes", async (req, res) => {
+  try {
+    //query db
+    const query = await db.query(
+      `SELECT id, recipe_name, ingredients, recipe_time, method, image_link FROM recipes;`
+    );
+    console.log(query.rows);
+    res.json(query.rows);
+  } catch (error) {
+    console.error(error, "Response failed. Try turning it off and on again.");
+    res.status(500).json({ response: "failed" });
+  }
+});
+
+// dynamic route (which wiill be nested in the static route above)
+app.get("/all-recipes/:id", async (req, res) => {
+  try {
+    //access the value of my id params
+    const { id } = req.params;
+    //query db
+    const query = await db.query(
+      `SELECT recipe_name, ingredients, recipe_time, method, image_link FROM recipes WHERE id = $1`,
+      [id]
+    );
+    console.log(query.rows);
+    res.json(query.rows);
+  } catch (error) {
+    console.error(error, "Response failed. Try turning it off and on again.");
+    res.status(500).json({ response: "failed" });
+  }
+});
+
+//* POST ROUTE
+// CREATE new recipes data
+// route --> http method POST
+// static route
+
+app.post("/add-recipe", (req, res) => {
+  try {
+    //collect the data to insert
+    const { recipe_name, ingredients, recipe_time, method, image_link } =
+      req.body;
+    //query the database to insert new data
+    const query = db.query(
+      `INSERT INTO recipes (recipe_name, ingredients, recipe_time, method, image_link) VALUES ($1, $2, $3,$4, $5) RETURNING *`,
+      [recipe_name, ingredients, recipe_time, method, image_link]
+    );
+    console.log(query.rows);
+    res.status(200).json({ request: "success" });
+  } catch (error) {
+    console.error(error, "Request failed. Turn off and on");
+    res.status(500).json({ request: "fail" });
+  }
+});
